71f412c0cb9a840b2766bae80941e4cb5de6a05b
Tim Kräuter <tim.kraeuterr@gmail.com>
2023-08-18 15:41:47 +0200

    Cleaned up write

--- test/spec/xml/write.js
+++ test/spec/xml/write.js
@@ -2,80 +2,25 @@
 
 var Helper = require("../../helper");
 
-xdescribe("write", function () {
+describe("write", function () {
   var moddle = Helper.createModdle();
 
   async function write(element) {
     return await moddle.toXML(element, { preamble: false });
   }
 
-  describe("should export types", function () {
-    it("Field#stringValue", async function () {
+  describe("should export", function () {
+    it("running processes", async function () {
       // given
-      var fieldElem = moddle.create("camunda:Field", {
-        name: "Field_1",
-        stringValue: "myFieldValue",
+      var element = moddle.create("bpmn:Process", {
+        runningProcess: ["1", "2", "3"],
       });
 
       var expectedXML =
-        '<camunda:field xmlns:camunda="http://camunda.org/schema/1.0/bpmn" ' +
-        'name="Field_1" stringValue="myFieldValue" />';
-
-      // when
-      var { xml } = await write(fieldElem);
-
-      // then
-      expect(xml).to.eql(expectedXML);
-    });
-
-    it("Field#string", async function () {
-      // given
-      var fieldElem = moddle.create("camunda:Field", {
-        name: "Field_1",
-        string: "myStringValue",
-      });
-
-      var expectedXML =
-        '<camunda:field xmlns:camunda="http://camunda.org/schema/1.0/bpmn" name="Field_1">' +
-        "<camunda:string>myStringValue</camunda:string>" +
-        "</camunda:field>";
-
-      // when
-      var { xml } = await write(fieldElem);
-
-      // then
-      expect(xml).to.eql(expectedXML);
-    });
-
-    it("Field#expression", async function () {
-      // given
-      var fieldElem = moddle.create("camunda:Field", {
-        name: "Field_1",
-        expression: "${myExpressionValue}",
-      });
-
-      var expectedXML =
-        '<camunda:field xmlns:camunda="http://camunda.org/schema/1.0/bpmn" name="Field_1">' +
-        "<camunda:expression>${myExpressionValue}</camunda:expression>" +
-        "</camunda:field>";
-
-      // when
-      var { xml } = await write(fieldElem);
-
-      // then
-      expect(xml).to.eql(expectedXML);
-    });
-
-    it("UserTask#formRefBinding", async function () {
-      // given
-      var element = moddle.create("bpmn:UserTask", {
-        "camunda:formRefBinding": "latest",
-      });
-
-      var expectedXML =
-        '<bpmn:userTask xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL" ' +
-        'xmlns:camunda="http://camunda.org/schema/1.0/bpmn" ' +
-        'camunda:formRefBinding="latest" />';
+        '<bpmn:process xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:token="http://tk/schema/1.0/token">' +
+        "<token:runningProcess>1</token:runningProcess>" +
+        "<token:runningProcess>2</token:runningProcess>" +
+        "<token:runningProcess>3</token:runningProcess></bpmn:process>";
 
       // when
       var { xml } = await write(element);
@@ -84,17 +29,36 @@ xdescribe("write", function () {
       expect(xml).to.eql(expectedXML);
     });
 
-    it("Process#ProcessInstance", async function () {
+    it("tokens at activities", async function () {
       // given
-      var element = moddle.create("bpmn:Process", {
-        runningProcess: ["1", "2", "3"],
+      var element = moddle.create("bpmn:Activity", {
+        token: ["1", "2", "3"],
       });
 
       var expectedXML =
-        '<bpmn:process xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:camunda="http://camunda.org/schema/1.0/bpmn">' +
-        "<camunda:runningProcess>1</camunda:runningProcess>" +
-        "<camunda:runningProcess>2</camunda:runningProcess>" +
-        "<camunda:runningProcess>3</camunda:runningProcess></bpmn:process>";
+        '<bpmn:activity xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:token="http://tk/schema/1.0/token">' +
+        "<token:token>1</token:token>" +
+        "<token:token>2</token:token>" +
+        "<token:token>3</token:token></bpmn:activity>";
+
+      // when
+      var { xml } = await write(element);
+
+      // then
+      expect(xml).to.eql(expectedXML);
+    });
+
+    it("tokens at sequence flows", async function () {
+      // given
+      var element = moddle.create("bpmn:SequenceFlow", {
+        token: ["1", "2", "3"],
+      });
+
+      var expectedXML =
+        '<bpmn:sequenceFlow xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:token="http://tk/schema/1.0/token">' +
+        "<token:token>1</token:token>" +
+        "<token:token>2</token:token>" +
+        "<token:token>3</token:token></bpmn:sequenceFlow>";
 
       // when
       var { xml } = await write(element);
