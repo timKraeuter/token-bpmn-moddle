93ea5b44ddccd475805bd881050938cd3c735aca
Tim Kräuter <tim.kraeuterr@gmail.com>
2023-08-29 10:14:42 +0200

    Add shouldExist boolean attribute to tokens

--- test/spec/xml/read.js
+++ test/spec/xml/read.js
@@ -1,67 +1,69 @@
 "use strict";
 
 const readFile = require("../../helper").readFile,
-  createModdle = require("../../helper").createModdle;
+    createModdle = require("../../helper").createModdle;
 
 describe("read", function () {
-  describe("should read extensions", function () {
-    const moddle = createModdle();
+    describe("should read extensions", function () {
+        const moddle = createModdle();
 
-    function read(xml, root, opts) {
-      return moddle.fromXML(xml, root, opts);
-    }
+        function read(xml, root, opts) {
+            return moddle.fromXML(xml, root, opts);
+        }
 
-    function fromFile(file, root, opts) {
-      const contents = readFile("test/fixtures/xml/" + file);
-      return read(contents, root, opts);
-    }
+        function fromFile(file, root, opts) {
+            const contents = readFile("test/fixtures/xml/" + file);
+            return read(contents, root, opts);
+        }
 
-    it("read process snapshot", async function () {
-      // given
-      const file = "snapshot.part.bpmn";
+        it("read process snapshot", async function () {
+            // given
+            const file = "snapshot.part.bpmn";
 
-      // when
-      const { rootElement: collaboration } = await fromFile(
-        file,
-        "bpmn:Collaboration",
-      );
+            // when
+            const {rootElement: collaboration} = await fromFile(
+                file,
+                "bpmn:Collaboration",
+            );
 
-      // then
-      expect(collaboration).to.jsonEqual({
-        $type: "bpmn:Collaboration",
-        id: "Collaboration_1qmax72",
-        artifacts: [
-          {
-            $type: "bt:ProcessSnapshot",
-            id: "ProcessSnapshot_1eoyn6s",
-          },
-        ],
-      });
+            // then
+            expect(collaboration).to.jsonEqual({
+                $type: "bpmn:Collaboration",
+                id: "Collaboration_1qmax72",
+                artifacts: [
+                    {
+                        $type: "bt:ProcessSnapshot",
+                        id: "ProcessSnapshot_1eoyn6s",
+                    },
+                ],
+            });
+        });
+
+        it("read tokens", async function () {
+            // given
+            const file = "token.part.bpmn";
+
+            // when
+            const {rootElement: process} = await fromFile(file, "bpmn:Process");
+
+            // then
+            expect(process).to.jsonEqual({
+                $type: "bpmn:Process",
+                id: "Process_1",
+                isExecutable: false,
+                artifacts: [
+                    {
+                        $type: "bt:Token",
+                        id: "Token_0iuqd2g",
+                        shouldExist: false
+
+                    },
+                    {
+                        $type: "bt:Token",
+                        id: "Token_1uiyzaw",
+                    },
+                ],
+            });
+        });
     });
-
-    it("read tokens", async function () {
-      // given
-      const file = "token.part.bpmn";
-
-      // when
-      const { rootElement: process } = await fromFile(file, "bpmn:Process");
-
-      // then
-      expect(process).to.jsonEqual({
-        $type: "bpmn:Process",
-        id: "Process_1",
-        isExecutable: false,
-        artifacts: [
-          {
-            $type: "bt:Token",
-            id: "Token_15kjo9s",
-          },
-          {
-            $type: "bt:Token",
-            id: "Token_1uiyzaw",
-          },
-        ],
-      });
-    });
-  });
 });
