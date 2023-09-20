3b79b92010ed24192814a07d34bb06657f62b680
Tim Kräuter <tim.kraeuterr@gmail.com>
2023-09-20 10:00:02 +0200

    Write extension elements

--- test/spec/xml/write.js
+++ test/spec/xml/write.js
@@ -6,26 +6,33 @@ describe("write", function () {
   var moddle = Helper.createModdle();
 
   async function write(element) {
-    return await moddle.toXML(element, {preamble: false});
+    return await moddle.toXML(element, { preamble: false });
   }
 
   describe("should export", function () {
     it("tokens with attributes", async function () {
       // given
+      const extensionElements = moddle.create("bpmn:ExtensionElements", {
+        values: [
+          moddle.create("bt:Token", { id: "1", shouldExist: false }),
+          moddle.create("bt:Token", { id: "2" }),
+        ],
+      });
+
       const element = moddle.create("bpmn:Process", {
-        artifacts: [moddle.create("bt:Token", {id: "1", shouldExist: false}),
-          moddle.create("bt:Token", {id: "2"})],
+        extensionElements,
       });
 
       const expectedXML =
-          '<bpmn:process xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bt="http://tk/schema/1.0/bt">'
-          +
-          "<bt:token id=\"1\" shouldExist=\"false\" />" +
-          "<bt:token id=\"2\" />" +
-          "</bpmn:process>";
+        '<bpmn:process xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bt="http://tk/schema/1.0/bt">' +
+        "<bpmn:extensionElements>" +
+        '<bt:token id="1" shouldExist="false" />' +
+        '<bt:token id="2" />' +
+        "</bpmn:extensionElements>" +
+        "</bpmn:process>";
 
       // when
-      const {xml} = await write(element);
+      const { xml } = await write(element);
 
       // then
       expect(xml).to.eql(expectedXML);
@@ -33,35 +40,49 @@ describe("write", function () {
 
     it("tokens with process snapshot references", async function () {
       // given
-      const snapshot1 = moddle.create("bt:ProcessSnapshot",
-          {id: "1"});
+      const snapshot = moddle.create("bt:ProcessSnapshot", { id: "1" });
+      const extensionElementsCollaboration = moddle.create(
+        "bpmn:ExtensionElements",
+        {
+          values: [snapshot],
+        },
+      );
+
+      const token = moddle.create("bt:Token", {
+        id: "1",
+        processSnapshot: snapshot,
+      });
+      const extensionElementsProcess = moddle.create("bpmn:ExtensionElements", {
+        values: [token],
+      });
+
       const defs = moddle.create("bpmn:Definitions", {
         rootElements: [
           moddle.create("bpmn:Collaboration", {
-            artifacts: [
-              snapshot1,
-            ],
+            extensionElements: extensionElementsCollaboration,
           }),
           moddle.create("bpmn:Process", {
-            artifacts: [moddle.create("bt:Token",
-                {id: "1", processSnapshot: snapshot1})],
+            extensionElements: extensionElementsProcess,
           }),
-        ]
+        ],
       });
-      // given
 
       const expectedXML =
-          "<bpmn:definitions xmlns:bpmn=\"http://www.omg.org/spec/BPMN/20100524/MODEL\" xmlns:bt=\"http://tk/schema/1.0/bt\">"
-          + "<bpmn:collaboration>"
-            + "<bt:processSnapshot id=\"1\" />"
-          + "</bpmn:collaboration>"
-          + "<bpmn:process>"
-            + "<bt:token id=\"1\" processSnapshot=\"1\" />"
-          + "</bpmn:process>"
-          + "</bpmn:definitions>";
+        '<bpmn:definitions xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bt="http://tk/schema/1.0/bt">' +
+        "<bpmn:collaboration>" +
+        "<bpmn:extensionElements>" +
+        '<bt:processSnapshot id="1" />' +
+        "</bpmn:extensionElements>" +
+        "</bpmn:collaboration>" +
+        "<bpmn:process>" +
+        "<bpmn:extensionElements>" +
+        '<bt:token id="1" processSnapshot="1" />' +
+        "</bpmn:extensionElements>" +
+        "</bpmn:process>" +
+        "</bpmn:definitions>";
 
       // when
-      const {xml} = await write(defs);
+      const { xml } = await write(defs);
 
       // then
       expect(xml).to.eql(expectedXML);
@@ -69,22 +90,26 @@ describe("write", function () {
 
     it("process snapshots", async function () {
       // given
-      const element = moddle.create("bpmn:Collaboration", {
-        artifacts: [
-          moddle.create("bt:ProcessSnapshot", {id: "1", shouldExist: false}),
-          moddle.create("bt:ProcessSnapshot", {id: "2"}),
+      const extensionElements = moddle.create("bpmn:ExtensionElements", {
+        values: [
+          moddle.create("bt:ProcessSnapshot", { id: "1", shouldExist: false }),
+          moddle.create("bt:ProcessSnapshot", { id: "2" }),
         ],
+      });
+      const element = moddle.create("bpmn:Collaboration", {
+        extensionElements,
       });
 
       const expectedXML =
-          '<bpmn:collaboration xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bt="http://tk/schema/1.0/bt">'
-          +
-          "<bt:processSnapshot id=\"1\" shouldExist=\"false\" />" +
-          "<bt:processSnapshot id=\"2\" />" +
-          "</bpmn:collaboration>";
+        '<bpmn:collaboration xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bt="http://tk/schema/1.0/bt">' +
+        "<bpmn:extensionElements>" +
+        '<bt:processSnapshot id="1" shouldExist="false" />' +
+        '<bt:processSnapshot id="2" />' +
+        "</bpmn:extensionElements>" +
+        "</bpmn:collaboration>";
 
       // when
-      const {xml} = await write(element);
+      const { xml } = await write(element);
 
       // then
       expect(xml).to.eql(expectedXML);
